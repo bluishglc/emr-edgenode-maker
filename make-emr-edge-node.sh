5e645b60dd5a28c94cb6972e4b34b965be81d8c3
buishglc <buishglc@126.com>
2020-09-12 10:48:32 +0800

    initial import

---
+++ make-emr-edge-node.sh
@@ -0,0 +1,216 @@
+#!/bin/sh
+
+# Run the below commands as root
+if [ "$USER" != "root" ]; then
+  echo "Run me with root user!"
+  exit 1
+fi
+
+showUsage() {
+  printHeading "[ MAKE EMR EDGE NODE SCRIPTS ] USAGE"
+
+  echo "# 说明：初始化（只需执行一次）, 安装基础软件包，emr基础app, 配置emr repo, 创建hadoop用户, 创建必要文件夹"
+  echo "$0 init [PEM_FILE_PATH] [MASTER_NODE_IP]"
+  echo
+
+  echo "# 说明：制作hadoop客户端"
+  echo "$0 make-hadoop-client [PEM_FILE_PATH] [MASTER_NODE_IP]"
+  echo
+
+  echo "# 说明：制作spark客户端"
+  echo "$0 make-spark-client [PEM_FILE_PATH] [MASTER_NODE_IP]"
+  echo
+
+  echo "# 说明：制作hive客户端"
+  echo "$0 make-hive-client [PEM_FILE_PATH] [MASTER_NODE_IP]"
+  echo
+
+  echo "# 说明：制作hbase客户端"
+  echo "$0 make-hbase-client [PEM_FILE_PATH] [MASTER_NODE_IP]"
+  echo
+
+  echo "# 说明：制作oozie客户端"
+  echo "$0 make-oozie-client [PEM_FILE_PATH] [MASTER_NODE_IP]"
+  echo
+
+  echo "# 说明：制作hudi客户端"
+  echo "$0 make-hudi-client"
+  echo
+
+  echo "# 说明：制作sqoop客户端"
+  echo "$0 make-sqoop-client"
+  echo
+}
+
+printHeading() {
+  title="$1"
+  paddingWidth=$((($(tput cols) - ${#title}) / 2 - 3))
+  printf "\n%${paddingWidth}s" | tr ' ' '='
+  printf "  $title  "
+  printf "%${paddingWidth}s\n\n" | tr ' ' '='
+}
+
+init() {
+  pemFile="$1"
+  masterNode="$2"
+  chmod 600 "$pemFile"
+  makeYumRepo "$pemFile" "$masterNode"
+  yum -y install vim wget zip unzip expect tree htop iotop nc telnet lrzsz openssl-devel emrfs emr-ddb emr-goodies emr-kinesis emr-s3-select emr-scripts
+  makeHadoopUser "$pemFile"
+  makeDir
+  makeJdk
+  echo "Plase REBOOT!!"
+}
+
+makeUser() {
+  # add group if not exists
+  user="$1"
+  group="$2"
+
+  egrep "^$group\:" /etc/group >&/dev/null
+  if [ "$?" != "0" ]; then
+    groupadd "$group"
+    echo "Group: $group is added."
+  fi
+
+  # add user if not exists and set password
+  egrep "^$user\:" /etc/passwd >&/dev/null
+  if [ "$?" != "0" ]; then
+    useradd -g "$group" "$user"
+    echo "User: $user is added."
+  fi
+  # enable all users of bdp group can as hdfs.
+  echo "$user ALL = (ALL) NOPASSWD: ALL" | tee /etc/sudoers.d/hadoop
+}
+
+makeHadoopUser() {
+  makeUser "hadoop" "hadoop"
+  mkdir -p /home/$user/.ssh
+  chown $user:$group /home/$user/.ssh
+  chmod 700 /home/$user/.ssh
+  cp $pemFile /home/$user/.ssh/id_isa
+  chown $user:$group /home/$user/.ssh/id_isa
+  chmod 600 /home/$user/.ssh/id_isa
+}
+
+makeYumRepo() {
+  pemFile="$1"
+  masterNode="$2"
+  scp -o StrictHostKeyChecking=no -i $pemFile hadoop@$masterNode:/etc/yum.repos.d/emr-apps.repo .
+  mv emr-apps.repo /etc/yum.repos.d/emr-apps.repo
+  scp -o StrictHostKeyChecking=no -i $pemFile hadoop@$masterNode:/var/aws/emr/repoPublicKey.txt .
+  mkdir -p /var/aws/emr/
+  mv repoPublicKey.txt /var/aws/emr/repoPublicKey.txt
+}
+
+makeDir() {
+  mkdir -p /mnt/s3
+  chmod 777 -R /mnt/s3
+  mkdir -p /mnt/tmp
+  chmod 777 -R /mnt/tmp
+}
+
+makeJdk() {
+  yum -y install java-1.8.0-openjdk
+  tee /etc/profile.d/java.sh <<EOF
+export JAVA_HOME=/etc/alternatives/jre
+export PATH=$JAVA_HOME/bin:$PATH
+EOF
+}
+
+makeHadoopClient() {
+  pemFile="$1"
+  masterNode="$2"
+  yum -y install hadoop-client hadoop-lzo
+  rsync -avz --delete -e "ssh -o StrictHostKeyChecking=no -o ServerAliveInterval=10 -i $pemFile" hadoop@$masterNode:'/etc/hadoop/conf/*' /etc/hadoop/conf
+}
+
+makeHiveClient() {
+  pemFile="$1"
+  masterNode="$2"
+  yum -y install tez hive hive-hcatalog
+  rsync -avz --delete -e "ssh -o StrictHostKeyChecking=no -o ServerAliveInterval=10 -i $pemFile" hadoop@$masterNode:'/etc/hive/conf/*' /etc/hive/conf
+  rsync -avz --delete -e "ssh -o StrictHostKeyChecking=no -o ServerAliveInterval=10 -i $pemFile" hadoop@$masterNode:'/etc/hive-hcatalog/conf/*' /etc/hive-hcatalog/conf
+  rsync -avz --delete -e "ssh -o StrictHostKeyChecking=no -o ServerAliveInterval=10 -i $pemFile" hadoop@$masterNode:'/etc/tez/conf/*' /etc/tez/conf
+  mkdir -p /var/log/hive/user
+  chmod 777 -R /var/log/hive/user
+}
+
+makeSparkClient() {
+  pemFile="$1"
+  masterNode="$2"
+  yum -y install spark-core spark-python spark-datanucleus
+  rsync -avz --delete -e "ssh -o StrictHostKeyChecking=no -o ServerAliveInterval=10 -i $pemFile" hadoop@$masterNode:'/etc/spark/conf/*' /etc/spark/conf
+  echo "spark.hadoop.yarn.timeline-service.enabled false" | tee -a /etc/spark/conf/spark-defaults.conf
+  mkdir -p /var/log/spark/user
+  chmod 777 -R /var/log/spark/user
+}
+
+makeHBaseClient() {
+  pemFile="$1"
+  masterNode="$2"
+  yum -y install hbase
+  rsync -avz --delete -e "ssh -o StrictHostKeyChecking=no -o ServerAliveInterval=10 -i $pemFile" hadoop@$masterNode:'/etc/hbase/conf/*' /etc/hbase/conf
+  mkdir -p /var/log/hbase
+  chmod 777 -R /var/log/hbase
+}
+
+makeHudiClient() {
+  yum -y install hudi
+}
+
+makeSqoopClient() {
+  yum -y install sqoop
+}
+
+makeOozieClient() {
+  pemFile="$1"
+  masterNode="$2"
+  yum -y install oozie # if install oozie-client only, oozie user won't create automatically.
+  rsync -avz --delete -e "ssh -o StrictHostKeyChecking=no -o ServerAliveInterval=10 -i $pemFile" hadoop@$masterNode:'/etc/oozie/conf/*' /etc/oozie/conf
+  mkdir -p /var/log/oozie
+  chmod 777 -R /var/log/oozie
+}
+
+case $1 in
+  init)
+    shift
+    init "$@"
+    ;;
+  make-hadoop-user)
+    shift
+    makeHadoopUser "$@"
+    ;;
+  make-hadoop-client)
+    shift
+    makeHadoopClient "$@"
+    ;;
+  make-hive-client)
+    shift
+    makeHiveClient "$@"
+    ;;
+  make-spark-client)
+    shift
+    makeSparkClient "$@"
+    ;;
+  make-hbase-client)
+    shift
+    makeHBaseClient "$@"
+    ;;
+  make-oozie-client)
+    shift
+    makeOozieClient "$@"
+    ;;
+  make-hudi-client)
+    makeHudiClient
+    ;;
+  make-sqoop-client)
+    makeSqoopClient
+    ;;
+  help)
+    showUsage
+    ;;
+  *)
+    showUsage
+    ;;
+esac
---
+++ readme.md
@@ -0,0 +1,51 @@
+﻿
+# 脚本使用方法
+
+**关于该脚本的详细介绍，请参考作者博客文章：[如何制作一个AWS EMR客户端节点（ Client Node / Edge Node / Gateway Node )]（https://laurence.blog.csdn.net/article/details/108529087）**
+
+检出后需要先给shell文件`make-emr-edge-node.sh`添加可执行属性：
+
+```bash
+chmod a+x make-emr-edge-node.sh
+```
+
+然后可以通过help选项查看使用方式：
+
+```bash
+sudo ./make-emr-edge-node.sh help
+
+==================  [ MAKE EMR EDGE NODE SCRIPTS ] USAGE  ==================
+
+# 说明：初始化（只需执行一次）, 安装基础软件包，emr基础app, 配置emr repo, 创建hadoop用户, 创建必要文件夹
+./make-emr-edge-node.sh init [PEM_FILE_PATH] [MASTER_NODE_IP]
+
+# 说明：制作hadoop客户端
+./make-emr-edge-node.sh make-hadoop-client [PEM_FILE_PATH] [MASTER_NODE_IP]
+
+# 说明：制作spark客户端
+./make-emr-edge-node.sh make-spark-client [PEM_FILE_PATH] [MASTER_NODE_IP]
+
+# 说明：制作hive客户端
+./make-emr-edge-node.sh make-hive-client [PEM_FILE_PATH] [MASTER_NODE_IP]
+
+# 说明：制作hbase客户端
+./make-emr-edge-node.sh make-hbase-client [PEM_FILE_PATH] [MASTER_NODE_IP]
+
+# 说明：制作oozie客户端
+./make-emr-edge-node.sh make-oozie-client [PEM_FILE_PATH] [MASTER_NODE_IP]
+
+# 说明：制作hudi客户端
+./make-emr-edge-node.sh make-hudi-client
+
+# 说明：制作sqoop客户端
+./make-emr-edge-node.sh make-sqoop-client
+
+```
+
+关于脚本的使用方式，如下几点需要注意：
+
+1. 由于脚本中大量操作需要使用root权限，所以必须以root用户或sudo方式运行脚本，否则脚本会给出错误提示
+2. 命令行中只涉及两个参数：
+- [PEM_FILE_PATH] ：你的AWS账号的PEM文件，执行脚本需要使用该秘钥文件登入Master节点拷贝配置文件；
+- [MASTER_NODE_IP]：你的Master节点IP或域名
+3. 在执行`make-*-client`操作前，需要先执行`init`操作（仅一次即可）
